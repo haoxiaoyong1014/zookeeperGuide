b0b99815fae2e30471bf0401159ab7938c65a7be
haoxiaoyong1014 <hxyHelloWorld@163.com>
2019-07-30 09:23:43 +0800

    add wacth

--- zk-curator-api/src/main/java/cn/haoxy/curator/api/watcher/WacthOperator.java
+++ zk-curator-api/src/main/java/cn/haoxy/curator/api/watcher/WacthOperator.java
@@ -4,6 +4,8 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.imps.CuratorFrameworkState;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
 import org.apache.curator.retry.RetryNTimes;
 
 import java.util.concurrent.TimeUnit;
@@ -62,12 +64,36 @@ public class WacthOperator {
          * STOPPED    已停止
          */
         //实例化
-        String nodePath = "/super/imooc";
+        final String nodePath = "/super/imooc";
         WacthOperator curatorConntion = new WacthOperator();
         CuratorFrameworkState state = curatorConntion.client.getState();
         System.out.println("当前客户的状态：" + (state == STARTED ? "已连接" : "已关闭"));
         //watcher事件,当使用 usingWatcher的时候,监听只会触发一次,监听完毕后就销毁
-        curatorConntion.client.getData().usingWatcher(new MyCuratorWatcher()).forPath(nodePath);
+        /*************监听只会触发一次***************/
+        //curatorConntion.client.getData().usingWatcher(new MyCuratorWatcher()).forPath(nodePath);
+
+
+        /*************一次监听N次触发***************/
+        //NodeCache:监听数据节点的变更,会触发事件
+        final NodeCache nodeCache = new NodeCache(curatorConntion.client, nodePath);
+        //buildInitial:初始化的时候获取node的值并且缓存,默认false,不缓存
+        nodeCache.start(true);
+        if (nodeCache.getCurrentData() != null) {
+            System.out.println("节点初始化数据为:" + new String(nodeCache.getCurrentData().getData()));
+        } else {
+            System.out.println("节点初始化数据为空....");
+        }
+        nodeCache.getListenable().addListener(new NodeCacheListener() {
+            public void nodeChanged() throws Exception {
+                if (nodeCache.getCurrentData() == null) {
+                    System.out.println("空节点");
+                    return;
+                }
+                String data = new String(nodeCache.getCurrentData().getData());
+                System.out.println("节点路径:" + nodeCache.getCurrentData().getPath() + "数据: " + data);
+            }
+        });
+
 
         TimeUnit.MINUTES.sleep(2);
         curatorConntion.closeZKClient();
